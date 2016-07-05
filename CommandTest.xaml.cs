e7b9fb36fbff676c7980dff540d6dfb0bdfc6e61
Alok Arya <alarya@syr.edu>
2016-07-05 00:01:22 -0700

    Initial commit

---
+++ CommandTest.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPFTestApp1
+{
+    /// <summary>
+    /// Interaction logic for CommandTest.xaml
+    /// </summary>
+    public partial class CommandTest : Window
+    {
+        public CommandTest()
+        {
+            InitializeComponent();
+
+            CommandBinding copyCommand = new CommandBinding(ApplicationCommands.Copy);
+
+            this.CommandBindings.Add(copyCommand);
+
+            copyCommand.Executed += new ExecutedRoutedEventHandler(copyCommand_Executed);
+
+        }
+        private void copyCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Debug.WriteLine("Copy Executed");
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace WPFTestApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //---------Test the radio button----------------//
+            //Window radioButtonTest = new RadioButtonTest();
+            //radioButtonTest.Show();
+
+            //---------Test Expander control---------------//
+            //Window expanderTest = new ExpanderTest();
+            //expanderTest.Show();
+
+            //---------Test Expander control---------------//
+            //Window textInputTest = new TextInputTest();
+            //textInputTest.Show();
+
+            //---------Test Expander control---------------//
+            //Window listBoxTest = new ListBoxTest();
+            //listBoxTest.Show();
+
+            //---------Test Menus and Commands---------------//
+            Window menusAndCommandsTest = new MenusAndCommandsTest();
+            menusAndCommandsTest.Show();
+
+        }
+    }
+}
